362f660525c95f1cb8259a06dc020d92eb01d15f
Ritesh <rkanoongo@gmail.com>
2017-05-05 15:17:30 +0530

    Payu Money Integration first complete round is done

--- database/payu_transaction_db.php
+++ database/payu_transaction_db.php
@@ -42,9 +42,17 @@ class PayuTransaction
 		
 	}
 	
-	public function getTransaction()	
+	public function getPayuTransaction($transaction_id)	
 	{
-		
+		$query = "select * from payu_transactions where transaction_id ='".  $transaction_id . "'" ;
+		//echo $query;
+		$result = mysql_query($query, $this->db_link) or die('Get payu transaction error : ' . mysql_error());
+		if(mysql_num_rows($result) > 0)
+			{
+				$row = mysql_fetch_array($result);
+				return $row;
+				
+			}
 		
 	}
 	
