20c83fc23807bcb5a8cb1f1831d9ae6dff48064b
Manish Arora <manish.mastishka@gmail.com>
2017-05-18 12:07:38 +0530

    restored google analytics

--- lib/header.php
+++ lib/header.php
@@ -44,32 +44,13 @@
 	}
 ?>
 	<script type="text/javascript">
-	<?php 
-	if(isset($from_free) && $from_free == 1)
-	{
-	?>
-		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-		ga('create', 'UA-2246912-21', 'auto');
-		ga('send', 'pageview');
-	<?php 
-	}
-	else 
-	{
-	?>
-		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-		ga('create', 'UA-2246912-20', 'auto');
-		ga('send', 'pageview');
-	<?php 
-	}
-	?>
+	  ga('create', 'UA-2246912-20', 'auto');
+	  ga('send', 'pageview');
 	</script>
 	
 	<div id='header' class="navbar navbar-inverse navbar-fixed-top">
--- test/mipcat.php
+++ test/mipcat.php
@@ -392,15 +392,13 @@
 			$objIncludeJsCSS->IncludeJqueryUI_1_12_1_JS("../");
 		?>
 		<script type="text/javascript">
-		  var _gaq = _gaq || [];
-		  _gaq.push(['_setAccount', 'UA-2246912-13']);
-		  _gaq.push(['_trackPageview']);
-		
-		  (function() {
-		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		  })();
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-2246912-13', 'auto');
+		  ga('send', 'pageview');
 		</script>
 		<script type="text/javascript" src="../js/mipcat/utils.js"></script>
 		<style type="text/css">
