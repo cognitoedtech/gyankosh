e65491d347fbabeedd269785e2d026bbff3ffd41
SunSha <1804576+SunSha@users.noreply.github.com>
2017-08-02 15:07:22 +0200

    Update index.php
    Updated verifyRequiredParams to handle json content type.

--- subdomain/rest/quizus_api/v1/index.php
+++ subdomain/rest/quizus_api/v1/index.php
@@ -211,7 +211,12 @@ function verifyRequiredParams($required_fields) {
         $app = \Slim\Slim::getInstance();
         parse_str($app->request()->getBody(), $request_params);
     }
-    foreach ($required_fields as $field) {
+	if($_SERVER["CONTENT_TYPE"] == 'application/json')
+	{
+	  $request_params = json_decode(file_get_contents('php://input'), true);
+	}
+	
+	foreach ($required_fields as $field) {
         if (!isset($request_params[$field]) || strlen(trim($request_params[$field])) <= 0) {
             $error = true;
             $error_fields .= $field . ', ';
