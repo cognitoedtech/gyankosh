8522a7c1fd3dc523fa7a27cba2d293438cd68f25
SunSha <1804576+SunSha@users.noreply.github.com>
2017-08-02 12:50:08 +0200

    Update index.php

--- subdomain/rest/quizus_api/v1/index.php
+++ subdomain/rest/quizus_api/v1/index.php
@@ -108,9 +108,31 @@ $app->post('/login', function() use ($app) {
             // check for correct email and password
 	
 	    $response['error'] = true;
-                $response['message'] = 'Check login failed';
+                $response['message'] = "Check login failed";
 	
+	    echoRespnse(200, $response);
+            if ($db->checkLogin($email, $password)) {
+                // get the user by email
+		    
+		
+                $user = $db->getUserByEmail($email);
 
+                if ($user != NULL) {
+                    $response["error"] = false;
+                    $response['name'] = $user['name'];
+                    $response['email'] = $user['email'];
+                    $response['apiKey'] = $user['api_key'];
+                    $response['user_id'] = $user['user_id'];
+                } else {
+                    // unknown error occurred
+                    $response['error'] = true;
+                    $response['message'] = "An error occurred. Please try again";
+                }
+            } else {
+                // user credentials are wrong
+                $response['error'] = true;
+                $response['message'] = 'Login failed. Incorrect credentials';
+            }
             echoRespnse(200, $response);
         });
 
